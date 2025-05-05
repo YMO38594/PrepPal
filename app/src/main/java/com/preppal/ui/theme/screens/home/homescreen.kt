4b253e4fc86758367727e83199a1a34ca8817e47
YMO38594 <oym38594@gmail.com>
2025-05-05 08:35:26 +0300

    Commit 2

--- app/src/main/java/com/preppal/ui/theme/screens/home/homescreen.kt
+++ app/src/main/java/com/preppal/ui/theme/screens/home/homescreen.kt
@@ -27,7 +27,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
